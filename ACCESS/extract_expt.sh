a35d5cfd56ad3e6f51ba7f30e1d76df01901abdb
EddieTianxingXu <tx97@cornell.edu>
2026-06-11 13:59:08 -0400

    gen expt images

--- ACCESS/extract_expt.sh
+++ ACCESS/extract_expt.sh
@@ -1,3 +1,36 @@
+#!/bin/bash
+#SBATCH --job-name="hsp90fin"
+#SBATCH --output="logs/run_%a.out"
+#SBATCH --error="logs/run_%a.err"
+#SBATCH --partition gpuA100x4
+#SBATCH --gpus=1
+#SBATCH --mem=120000M
+#SBATCH --export=ALL
+#SBATCH --time=1:00:00
+#SBATCH --array=10%1
+#SBATCH --account=bfly-delta-gpu
+
+unset LD_LIBRARY_PATH
+source /u/txu8/miniconda3/etc/profile.d/conda.sh
+
+cd /u/txu8/project/cryojax-ensemble-optimization/ACCESS
+
+conda activate cryojax_eo_env
+export TF_GPU_ALLOCATOR=cuda_malloc_async 
+export XLA_FLAGS="--xla_gpu_autotune_level=0 --xla_gpu_enable_triton_gemm=false"
+export PYTHONPATH=$PYTHONPATH:/u/txu8/project/github/package-update/src
+
+COMMANDS=(
+    "python -m get_sim -sbatch 512 --expt_dir /work/hdd/bfly/txu8/project/HSP90-p23/half1trunc -star /work/hdd/bfly/txu8/project/HSP90-p23/star/starfile_half1_truncate.star -mrc /work/hdd/bfly/txu8/project/HSP90-p23/particles/"
+)
+
+
+export PYTHONFAULTHANDLER=1
+
+MAPPED_ID=$((SLURM_ARRAY_TASK_ID-10))
+
+eval "${COMMANDS[$MAPPED_ID]}"
+
 #for local 
 
 # python -m extract_expt -mrc /home/eddie/Documents/ -star /home/eddie/Documents/STAR/EMD-50421_particles_truncate.star --expt_dir ./mrc_images
